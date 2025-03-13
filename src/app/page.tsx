f600c6d0df74429df86bd7ec7603d24936881746
Dilukshan <dilukshan2020da@gmail.com>
2025-03-13 11:24:17 +0530

    create hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center">
+    <div className="flex flex-col h-screen justify-center items-center">
       <h1 className="text-2xl md:text-4xl font-bold">Google Authentication</h1>
+      <Hero/>
     </div>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from 'react'
+import { Button2 } from './ui/Button2'
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import {auth, logout} from "../../lib/auth"
 
 const Hero = () => {
+    const [user, setUser] = useState<any>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+          setUser(user);
+        });
+        return () => unsubscribe();
+      }, []);
+
+      const handleAuthAction = async () => {
+        if (user) {
+          await logout();
+        } else {
+          router.push("/login"); // Redirect to login page
+        }
+      };
+
   return (
-    <div>Hero</div>
+    <div>
+        <Button2
+          variant="destructive"
+          className="h-9 bg-[#1175BC] hover:bg-[#0B2838] text-white flex items-center rounded-[14px]"
+          onClick={handleAuthAction}
+        >
+          {user ? "Logout" : "Login/Signup"} <MdKeyboardDoubleArrowRight className="h-6 w-6" />
+        </Button2>
+    </div>
   )
 }
 
